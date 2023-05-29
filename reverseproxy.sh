a0c3460ef4bda5a32e46debcad024917d7492fae
Joko Prasetyo <92915640+7oxP@users.noreply.github.com>
2023-05-29 19:02:56 +0700

    Create reverseproxy.sh
    A script for adding new user, then installing and configuring Nginx Reverse Proxy semi-automatically

---
+++ reverseproxy.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+set -eo pipefail
+
+if [ $(id -u) -eq 0 ]; then
+
+    read -p "Masukkan nama username baru: " user
+
+    if id "$user"> /dev/null 2>&1; then
+        echo "Pengguna dengan nama $user sudah ada."
+    else
+        echo "Pengguna dengan nama $user belum ada. Membuat pengguna..."
+        echo ""
+        #Membuat pengguna baru dengan nama tertentu
+        adduser $user --gecos ",,," --disabled-password
+        echo ""
+        echo "Pengguna dengan nama $user berhasil dibuat."
+    fi
+
+    # Melakukan pengecekkan terhadap program Nginx
+    if ! command -v nginx; then
+        echo ""
+        echo "Nginx belum terpasang. Memulai proses pengunduhan..."
+        echo ""
+
+        apt-get install nginx-core -y
+        echo ""
+        echo "Nginx berhasil diunduh dan diinstal."
+    else
+        echo "Nginx sudah terpasang."
+    fi
+
+    echo "Melakukan konfigurasi Reverse proxy..."
+    echo ""
+
+    read -p "Masukkan alamat IP dan Port: " alamat
+    echo ""
+    
+    unlink /etc/nginx/sites-enabled/default
+    path="/etc/nginx/sites-available/flask-app"
+    touch $path
+    
+    cat > $path << EOF
+    server {
+        listen 80;
+        location /$user {
+            proxy_pass http://$alamat;
+        }
+    }
+    EOF
+
+    # Membuat symlink ke direktori sites-enabled
+    ln -s $path /etc/nginx/sites-enabled/
+
+    service nginx restart
+
+    echo "Konfigurasi selesai!"
+    echo "Hasil konfigurasi dapat dilihat pada localhost'/'$user"
+    
+else
+    echo "Login sebagai root untuk menjalankan script ini"
+fi
